296b819f4f73d984c47c66406a8e1b166838a30c
Bergerat Santiago <70027319+Yago030@users.noreply.github.com>
2024-02-29 16:37:27 -0300

    subiendo arreglos , dos funcionalidad lista

--- appgoal/src/components/SystemsPage/SystemsPage.tsx
+++ appgoal/src/components/SystemsPage/SystemsPage.tsx
@@ -1,26 +1,47 @@
-// systemsPage.tsx
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+
 const SystemsPage: React.FC = () => {
+  const router = useRouter();
   const [assets, setAssets] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchAssets = async () => {
+    const checkAuthentication = async () => {
       try {
-        const response = await fetch('https://api.saldo.com.ar/v3/systems'); //sacandole el corsaniwhere anduvo aqui, 
-        if (response.ok) {
-          const data = await response.json();
-          setAssets(data.data); // Asegúrate de asignar data.data para obtener el array de activos
+        const isLoggedIn = localStorage.getItem('isLoggedIn'); // Obtener el estado de autenticación
+        if (isLoggedIn === 'true') {
+          await fetchAssets(); // Cargar los activos si está autenticado
         } else {
-          console.error('Error al obtener la lista de activos');
+          router.push('/login'); // Redirigir al usuario a /login si no está autenticado
         }
       } catch (error) {
-        console.error('Error al obtener la lista de activos:', error);
+        console.error('Error al verificar la autenticación:', error);
       }
     };
 
-    fetchAssets();
-  }, []);
+    checkAuthentication(); // Llamar a la función de verificación de autenticación al cargar el componente
+  }, [router]);
+
+  const fetchAssets = async () => {
+    try {
+      const response = await fetch('https://api.saldo.com.ar/v3/systems');
+      if (response.ok) {
+        const data = await response.json();
+        setAssets(data.data); // Asegúrate de asignar data.data para obtener el array de activos
+      } else {
+        console.error('Error al obtener la lista de activos');
+      }
+    } catch (error) {
+      console.error('Error al obtener la lista de activos:', error);
+    } finally {
+      setLoading(false); // Establecer loading en false después de cargar los activos
+    }
+  };
+
+  if (loading) {
+    return <div>Cargando...</div>; // Mostrar un mensaje de carga mientras se carga la lista de activos
+  }
 
   return (
     <div className="container mx-auto px-4 py-8 text-lg">
@@ -29,7 +50,7 @@ const SystemsPage: React.FC = () => {
       <ul className="grid grid-cols-4 gap-4">
         {assets.length > 0 ? (
           assets.map((asset: any) => (
-            <li key={asset.id} className="bg-cyan-500 p-4 rounded-md shadow-md  ">
+            <li key={asset.id} className="bg-cyan-500 bg-00CC99 p-4 rounded-md shadow-md">
               <span className="text-lg">{asset.attributes.name}</span>
             </li>
           ))
